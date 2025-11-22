97d13cda73fcd1290b1e441ff28cb6c919127820
JusticeBrains <justiceduodu14@gmail.com>
2025-11-22 09:34:23 +0000

    Enum types added

---
+++ utils/text_options.py
@@ -0,0 +1,20 @@
+from enum import StrEnum
+
+
+class InterestCalculationType(StrEnum):
+    FLAT = "Flat Rate"
+    AMORTIZATION = "Amortization"
+    TERM = "Loan Term"
+    REDUCING = "Reducing Balance"
+    EQUAL_PAYMENT = "Reducing Balance (Equal Payment)"
+    STRAIGHT_LINE = "Straight Line"
+
+
+class InterestTerm(StrEnum):
+    PER_ANNUM = "Per Annum"
+    PER_MONTH = "Per Month"
+
+
+class PaymentType(StrEnum):
+    DEFAULT = "Default"
+    CUSTOM = "Custom"
