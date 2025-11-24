3225d5cedcfd47ccbbd787f80868fb2a5058d985
JusticeBrains <justiceduodu14@gmail.com>
2025-11-24 23:04:46 +0000

    gunicorn conf file added

---
+++ gunicorn.conf.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import os
+
+
+bind = "0.0.0.0:8000"
+
+workers_per_core = float(os.getenv("WORKERS_PER_CORE", "1"))
+cores = multiprocessing.cpu_count()
+default_web_concurrency = workers_per_core * cores + 1
+workers = int(os.getenv("WEB_CONCURRENCY", default_web_concurrency))
+
+worker_class = "uvicorn.workers.UvicornWorker"
+
+keepalive = 120
+timeout = 120
+graceful_timeout = 120
+
+loglevel = os.getenv("LOG_LEVEL", "info")
+accesslog = "-"
+errorlog = "-"
