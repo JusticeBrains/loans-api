a32fab498d598d809db89a758879b35cf39a9a94
JusticeBrains <justiceduodu14@gmail.com>
2025-11-24 22:58:39 +0000

    changed hashing algorithm

--- utils/crypto.py
+++ utils/crypto.py
@@ -1,12 +1,27 @@
-from passlib.context import CryptContext
-
-pwd_hasher = CryptContext(schemes=["argon2"])
+import bcrypt
 
 
-def hash_password(password: str):
-    return pwd_hasher.hash(password)
+def truncate(password: str) -> bytes:
+    return password.encode("utf-8")[:72]
 
 
-def verify_password(password: str, hashed_password: str):
-    return pwd_hasher.verify(password, hashed_password)
+def hash_password(password: str) -> str:
+    hashed = bcrypt.hashpw(truncate(password), bcrypt.gensalt())
+    return hashed.decode()
 
+
+def verify_password(password: str, hashed: str) -> bool:
+    return bcrypt.checkpw(truncate(password), hashed.encode())
+
+
+# from passlib.context import CryptContext
+
+# pwd_hasher = CryptContext(schemes=["argon2"])
+
+
+# def hash_password(password: str):
+#     return pwd_hasher.hash(password)
+
+
+# def verify_password(password: str, hashed_password: str):
+#     return pwd_hasher.verify(password, hashed_password)
