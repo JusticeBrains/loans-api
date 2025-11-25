69da6a90bf2dc62fc5bbabdf8dbc682da8ce1954
JusticeBrains <justiceduodu14@gmail.com>
2025-11-25 02:39:48 +0000

    Update base.py

--- schemas/base.py
+++ schemas/base.py
@@ -1,10 +1,9 @@
-from typing import Generic, TypeVar, List
-from datetime import datetime
-
+from typing import Generic, List, TypeVar
 from sqlmodel import SQLModel
 
+T = TypeVar("T")
 
-class ResponseModel(SQLModel, Generic[TypeVar("T")]):
+class ResponseModel(SQLModel, Generic[T]):
     count: int
     next: str | None = None
     previous: str | None = None
