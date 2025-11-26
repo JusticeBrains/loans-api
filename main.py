a3ef8f74e424e2685fed2766ad1632b187960730
JusticeBrains <justiceduodu14@gmail.com>
2025-11-26 01:11:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from api.router import api_router
-
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Loans API", version="1.0.1")
 
+Instrumentator().instrument(app).expose(app)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_headers=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_credentials=False,
+    allow_headers=["*"],
 )
+
 
 app.add_middleware(
     TrustedHostMiddleware,
